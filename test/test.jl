c384f7c78b546a794795a35fce23cfd7b00f6428
Carlo Baldassi <carlobaldassi@gmail.com>
2013-05-14 17:04:54 +0200

    Fix tests

--- test/test.jl
+++ test/test.jl
@@ -1,3 +1,6 @@
+using TextWrap
+using OptionsMod
+
 text = "Julia is a high-level, high-performance dynamic programming language " *
        "for technical computing, with syntax that is familiar to users of " *
        "other technical computing environments. It provides a sophisticated " *
