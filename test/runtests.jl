197c4a7ab321621841acdfdd844d4c4f6a1fe4b8
Carlo Baldassi <carlobaldassi@gmail.com>
2014-08-05 14:51:45 +0200

    Yet more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,7 +59,7 @@ text = """
     and an extensive mathematical
     function library."""
 
-tabtext = "aaaaaaa\tbbbbbbb\tcccccc\tddddd\teeee\tfff\tgg\th\n"
+tabtext = "aaaaaaa\tbbbbbbb\t\ncccccc\tddddd\teeee\tfff\tgg\th\n"
 
 @test wrap(tabtext, width=20, replace_whitespace=true,  expand_tabs=true) ==
     "aaaaaaa bbbbbbb\ncccccc  ddddd   eeee\nfff     gg      h"
@@ -308,8 +308,61 @@ try
         execution, numerical accuracy,
         and an extensive mathematical
         function library."""
+
+    open(tmpf, "w") do f
+        println_wrapped(f, text, width=30, fix_sentence_endings=true, break_on_hyphens=false)
+    end
+    @test readall(tmpf) == """
+        Julia is a high-level,
+        high-performance dynamic
+        programming language for
+        technical computing, with
+        syntax that is familiar to
+        users of other technical
+        computing environments.  It
+        provides a sophisticated
+        compiler, distributed parallel
+        execution, numerical accuracy,
+        and an extensive mathematical
+        function library.
+        """
+
+    open(tmpf, "w") do f
+        println_wrapped(f, width=30, fix_sentence_endings=true, break_on_hyphens=false)
+    end
+    @test readall(tmpf) == "\n"
+
+    if VERSION >= v"0.3-"
+        open(tmpf, "w") do f
+            bk_STDOUT = STDOUT
+            try
+                redirect_stdout(f)
+                print_wrapped(text, width=30, fix_sentence_endings=true, break_on_hyphens=false)
+            finally
+                redirect_stdout(bk_STDOUT)
+            end
+        end
+        @test readall(tmpf) == """
+            Julia is a high-level,
+            high-performance dynamic
+            programming language for
+            technical computing, with
+            syntax that is familiar to
+            users of other technical
+            computing environments.  It
+            provides a sophisticated
+            compiler, distributed parallel
+            execution, numerical accuracy,
+            and an extensive mathematical
+            function library."""
+    end
 finally
     isfile(tmpf) && rm(tmpf)
+end
+
+if VERSION >= v"0.3-"
+    @test_throws ErrorException wrap("", initial_indent=10, width=10)
+    @test_throws ErrorException wrap("", subsequent_indent=10, width=10)
 end
 
 end
